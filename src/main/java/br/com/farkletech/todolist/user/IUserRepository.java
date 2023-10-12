fea688e978c0934f197a8212bfd11107f62b2a37
ViniciusNeivas <viniciusneivas@outlook.com.br>
2023-10-12 17:27:23 -0300

    Feat:[Início da construção do back-end da aplicação e integração com Banco de Dados H2 em memória]

---
+++ src/main/java/br/com/farkletech/todolist/user/IUserRepository.java
@@ -0,0 +1,11 @@
+package br.com.farkletech.todolist.user;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+
+public interface IUserRepository extends JpaRepository<UserModel, UUID>{
+    UserModel findByUsername(String username);
+}
