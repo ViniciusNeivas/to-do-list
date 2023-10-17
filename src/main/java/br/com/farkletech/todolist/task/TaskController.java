1cf01122f9ac911f59bcb52582a09b8160023d0a
ViniciusNeivas <viniciusneivas@outlook.com.br>
2023-10-17 15:11:27 -0300

    Feat:[Criação da validação de datas para evitar inserção de dados incoerentes por parte do usuário]

--- src/main/java/br/com/farkletech/todolist/task/TaskController.java
+++ src/main/java/br/com/farkletech/todolist/task/TaskController.java
@@ -1,8 +1,12 @@
 package br.com.farkletech.todolist.task;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,11 +22,20 @@ public class TaskController {
     private ITaskRepository taskRepository;
 
     @PostMapping("/")
-    public TaskModel create(@RequestBody TaskModel taskModel, HttpServletRequest request){
+    public ResponseEntity create(@RequestBody TaskModel taskModel, HttpServletRequest request){
         // System.out.println("Chegou no controller " + idUser);
         var idUser = request.getAttribute("idUser");
         taskModel.setIdUser((UUID)idUser);
+        var currentDate = LocalDateTime.now();
+        if(currentDate.isAfter(taskModel.getStartAt()) || currentDate.isAfter(taskModel.getEndAt())){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body("A data de início e data de termínio devem ser maiores do que a data atual!");
+        }
+        if(taskModel.getStartAt().isAfter(taskModel.getEndAt())){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body("A data de início dever ser menor do que a data de términio!");
+        }
         var task = this.taskRepository.save(taskModel);
-        return task;
+        return ResponseEntity.status(HttpStatus.OK).body(task);
     }
 }
