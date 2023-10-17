2aa1a33ff68bea247dce93863ac1419f3250cbbd
ViniciusNeivas <viniciusneivas@outlook.com.br>
2023-10-17 18:54:59 -0300

    Feat: [Implementação de atualização das tarefas (tasks), podendo atualizar separadamente os atributos.]

--- src/main/java/br/com/farkletech/todolist/task/TaskController.java
+++ src/main/java/br/com/farkletech/todolist/task/TaskController.java
@@ -1,6 +1,5 @@
 package br.com.farkletech.todolist.task;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
@@ -9,11 +8,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.farkletech.todolist.utils.Utils;
 import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
@@ -46,5 +48,12 @@ public class TaskController {
         var idUser = request.getAttribute("idUser");
         var tasks = this.taskRepository.findByIdUser((UUID) idUser);
         return tasks;
+    }
+
+    @PutMapping("/{id}")
+    public TaskModel update(@RequestBody TaskModel taskModel, @PathVariable UUID id, HttpServletRequest request){
+        var task = this.taskRepository.findById(id).orElse(null);
+        Utils.copyNonNullProperties(taskModel, task);
+        return this.taskRepository.save(task);
     }
 }
---
+++ src/main/java/br/com/farkletech/todolist/utils/Utils.java
@@ -0,0 +1,34 @@
+package br.com.farkletech.todolist.utils;
+
+import java.beans.PropertyDescriptor;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.BeanWrapper;
+import org.springframework.beans.BeanWrapperImpl;
+import org.springframework.context.annotation.Bean;
+
+public class Utils {
+
+    public static void copyNonNullProperties(Object source, Object target){
+        BeanUtils.copyProperties(source, target, getNullPropertyNames(source));
+    }
+
+    public static String[] getNullPropertyNames(Object source) {
+        final BeanWrapper src = new BeanWrapperImpl(source);
+
+        PropertyDescriptor[] pds = src.getPropertyDescriptors();
+
+        Set<String> emptyNames = new HashSet<>();
+
+        for (PropertyDescriptor pd : pds) {
+            Object srcValeu = src.getPropertyValue(pd.getName());
+            if (srcValeu == null) {
+                emptyNames.add(pd.getName());
+            }
+        }
+       String[] result = new String[emptyNames.size()];
+       return emptyNames.toArray(result);
+    }
+}
